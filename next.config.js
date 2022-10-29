394e164ec9bd78ea335fc17aad65317ba325e300
Ewoigift <ewoigift@gmail.com>
2022-10-29 14:48:16 +0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    domains: ["www.google.com", "img1.pnghut.com", "assets.stickpng.com", "e7.pngegg.com"],
+    domains: ["www.google.com", "img1.pnghut.com", "assets.stickpng.com", "e7.pngegg.com","static01.nyt.com"],
   },
 }; 
